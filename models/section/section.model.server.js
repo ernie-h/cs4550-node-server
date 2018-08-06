a27b27b832deef9e2c59a53fbb7a3ded0b74e1dc
ernie-hao <pdphiro@gmail.com>
2018-08-06 14:29:33 -0400

    added section delete

--- models/section/section.model.server.js
+++ models/section/section.model.server.js
@@ -4,25 +4,32 @@ const sectionSchema = require('./section.schema.server');
 const sectionModel = mongoose.model('SectionModel', sectionSchema);
 const userModel = require('../user/user.model.server');
 
+
+
 findAllSections = () =>
   sectionModel.find();
 
 findAllSectionsForCourse = courseId =>
-  sectionModel.find({courseId: courseId});
-
-createSection = section =>
-  sectionModel.create(section);
+  sectionModel.find({
+    courseId: courseId
+  });
 
 enroll = (userId, sectionId) =>
   userModel.findUserById(userId)
-    .then(user => {
-      user.sections.push(sectionId);
-      return user.save();
-    })
+  .then(user => {
+    user.sections.push(sectionId);
+    return user.save();
+  })
+createSection = section =>
+  sectionModel.create(section);
+
+deleteSection = (sectionId) =>
+  sectionModel.deleteOne({_id:  sectionId});
 
 module.exports = {
   enroll,
   findAllSections,
   findAllSectionsForCourse,
-  createSection
+  createSection,
+  deleteSection,
 };
--- services/section.service.server.js
+++ services/section.service.server.js
@@ -1,11 +1,6 @@
 module.exports = app => {
   const sectionModel = require('../models/section/section.model.server');
 
-  app.put('/api/section/:sectionId/enroll', (req, res) => {
-    const currentUser = req.session['currentUser'];
-    sectionModel.enroll(currentUser._id, req.params['sectionId'])
-      .then(status => res.sendStatus(200))
-  });
 
   app.get('/api/section', (req, res) =>
     sectionModel.findAllSections()
@@ -16,9 +11,19 @@ module.exports = app => {
     sectionModel.findAllSectionsForCourse(req.params['courseId'])
       .then(sections => res.send(sections))
   );
+  app.put('/api/section/:sectionId/enroll', (req, res) => {
+    const currentUser = req.session['currentUser'];
+    sectionModel.enroll(currentUser._id, req.params['sectionId'])
+    .then(status => res.sendStatus(status));
+  });
 
   app.post('/api/section', (req, res) =>
     sectionModel.createSection(req.body)
       .then(section => res.send(section))
+  );
+
+  app.delete('/api/section/:sectionId', (req, res) =>
+    sectionModel.deleteSection(req.params['sectionId'])
+      .then(response => res.send(response))
   );
 };
